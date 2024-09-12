dd734b51ab016ec3569f433c6994e611d78198b9
Liedson Mendes <mendesliedson978@gmail.com>
2024-09-12 18:26:26 -0300

    create challenge somaIndice

---
+++ execicies-target/src/main/java/org/example/SomaIndice.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class SomaIndice {
+
+    public static void main(String[] args) {
+
+        int INDICE = 12, SOMA = 0, K = 1;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println("O valor de SOMA é: " + SOMA);
+    }
+}
